0687c21caf19ae39ca3cb2e94bb7025555305982
Dakshkumar07 <74961250+Dakshkumar07@users.noreply.github.com>
2020-12-29 16:07:03 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Constrainede
+# angryBirdsStage2StudentActivity
+Student Activity Link for Angry Birds Stage 2
---
+++ chain.js
@@ -0,0 +1,21 @@
+class Chain {
+        constructor(bodyA,bodyB){
+       var option ={
+            bodyA:bodyA,
+            bodyB:bodyB,
+            stiffness :0.04,
+            lenght :10 
+        }
+
+        this.chain = Constraint.create(option);
+        World.add(World,this.chain);
+        
+        }
+        display(){
+            var pointA = this.chain.bodyA.position
+            var pointB = this.chain.bodyB.position
+            strokeWeight(4);
+            line(pointA.x,pointA.y,pointB.x,pointB.y)
+        }
+
+}
